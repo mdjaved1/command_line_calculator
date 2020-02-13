902a2a1fabb20362df28a17eb180f19a9948b921
Ubuntu <ubuntu@ip-172-31-46-197.ec2.internal>
2020-02-13 04:03:55 +0000

    committing first commit  after 24 hours code found in caculator.rb

---
+++ Calculator.rb
@@ -0,0 +1,34 @@
+class Calculator
+  
+  def evaluate(string)
+    arradd = []
+    arrmul = []
+    arrsub = []
+    arrdiv = []
+    str_i = string.split
+    
+    str_i.each_with_index do |ele , index|
+      if ele == "+"
+        arradd <<  str_i[index - 1] 
+        arradd <<  str_i[index + 1] 
+        return arradd.map(&:to_i).reduce(0, :+)
+      elsif ele == "*"
+        arrmul << str_i[index - 1] 
+        arrmul << str_i[index + 1] 
+        return arrmul.map(&:to_i).inject(:*)
+      elsif ele == "-"
+        arrsub <<  str_i[index - 1] 
+        arrsub <<  str_i[index + 1]    
+        return arrsub.map(&:to_i).inject(:-)
+      elsif ele == "/"
+        arrdiv << str_i[index - 1]
+        arrdiv << str_i[index + 1].to_f 
+        puts arrdiv
+        return arrdiv.map(&:to_i).reduce(:/)
+      else
+      
+      end
+    end
+    
+  end
+end

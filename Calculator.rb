fe2d59327a04d8c1b2863eeeed79f25c914bda4b
Ubuntu <ubuntu@ip-172-31-46-197.ec2.internal>
2020-02-20 03:02:03 +0000

    revamp quick simple calculator please use space between numbers and operator

--- Calculator.rb
+++ Calculator.rb
@@ -1,35 +1,18 @@
-class Calculator
-  
-  def evaluate(string)
-    arradd = []
-    arrmul = []
-    arrsub = []
-    arrdiv = []
-    str_i = string.split
-    
-    str_i.each_with_index do |ele , index|
-      if ele == "+"
-        arradd <<  str_i[index - 1] 
-        arradd <<  str_i[index + 1] 
-        return arradd.map(&:to_i).reduce(0, :+)
-      elsif ele == "*"
-        arrmul << str_i[index - 1] 
-        arrmul << str_i[index + 1] 
-        return arrmul.map(&:to_i).inject(:*)
-      elsif ele == "-"
-        arrsub <<  str_i[index - 1] 
-        arrsub <<  str_i[index + 1]    
-        return arrsub.map(&:to_i).inject(:-)
-      elsif ele == "/"
-        arrdiv << str_i[index - 1]
-        arrdiv << str_i[index + 1].to_f 
-        puts arrdiv
-        return arrdiv.map(&:to_i).reduce(:/)
-        
-      else
-      
-      end
-    end
-    
-  end
-end
+puts "as simple of a calc as it gets "
+print ">>"
+input = gets.chomp.split(" ")
+operand1 = input[0].to_i
+operand2 = input[2].to_i
+operator = input[1].to_sym
+
+
+result = case operator
+   when :+ then operand1 + operand2
+   when :- then operand1 - operand2
+   when :* then operand1 * operand2
+   when :/ then operand1 / operand2
+   when :% then operand1 % operand2   
+   else 
+     nil
+   end
+ puts result  
